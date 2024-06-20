1ad7fb90a593b2ae4623e6503b5e96994ae9d681
IdaCy <ifc00007@students.stir.ac.uk>
2024-06-20 16:56:23 +0200

    FULLY WORKING VERSION - simple compare test successful

--- tree/ntupleutil/v7/src/RNTupleTTreeCheckerCLI.cxx
+++ tree/ntupleutil/v7/src/RNTupleTTreeCheckerCLI.cxx
@@ -31,6 +31,8 @@ namespace Experimental {
 namespace RNTupleTTreeCheckerCLI {
 
 CheckerConfig ParseArgs(const std::vector<std::string> &args) {
+	std::cout << "Total args: " << args.size() << std::endl;
+
     const auto argsProvided = args.size() >= 2;
     const auto helpUsed = argsProvided && (args[1] == "--help" || args[1] == "-h");
 
@@ -46,6 +48,7 @@ CheckerConfig ParseArgs(const std::vector<std::string> &args) {
 
     for (size_t i = 1; i < args.size(); ++i) {
         const auto &arg = args[i];
+		std::cout << "Processing arg: " << i << std::endl;
 
         if (arg == "--ttree" || arg == "-t") {
             if (++i < args.size()) config.fTTreeFile = args[i];
